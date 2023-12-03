3be6f98bda9e4918621275c080562075cadafe1d
gedennis <gedennis@163.com>
2023-12-04 00:11:03 +0800

    feat(full-cluster): add scripts to set up full-cluster

---
+++ scripts/install_docker.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+set -euo pipefail
+
+if [ ! "$(command -v docker)" ]; then
+    # enable docker remote access outside the VM
+    mkdir -p /etc/systemd/system/docker.service.d
+    cat >/etc/systemd/system/docker.service.d/override.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375 --containerd=/run/containerd/containerd.sock
+EOF
+
+    # Refer: https://docs.docker.com/engine/install/ubuntu/
+    # Add Docker's official GPG key:
+    sudo apt-get install 
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    sudo apt-get update -y
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker vagrant
+    newgrp docker
+else 
+    echo "docker has already been installed."
+fi
+docker version
---
+++ scripts/install_tools.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+set -euo pipefail
+
+echo "start to install basic tools ..."
+apt-get update -y
+apt-get install -y git vim curl jq wget ca-certificates gnupg build-essential
+
+echo "install basic tools done."
